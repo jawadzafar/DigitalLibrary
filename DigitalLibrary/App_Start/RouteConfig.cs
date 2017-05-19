3dbd14f06abe9b22ad12e4763d579d981960f026
Khurram Shahzad <Khurram Shahzad>
2017-05-19 13:27:49 +0500

    Added Index page with design

--- DigitalLibrary/App_Start/RouteConfig.cs
+++ DigitalLibrary/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DigitalLibrary
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "DigitalLibrary", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
