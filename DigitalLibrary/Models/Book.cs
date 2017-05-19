60cb3b91fbc6b93d935ab47ee97caaa77b96bf10
Jawad Khan <Jawad Khan>
2017-05-19 13:24:41 +0500

    Models Commit

---
+++ DigitalLibrary/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigitalLibrary.Models
+{
+    public class Book
+    {
+        [DontInsert]
+        [DontUpdate]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AuthorId { get; set; }
+        public int PublisherId { get; set; }
+        public DateTime PublishingYear { get; set; }
+        public int EditionNumber { get; set; }
+        public int BookCompleted { get; set; }
+        public int NoOfPages { get; set; }
+        public int NoOfAbwaabs { get; set; }
+    }
+}
