2e4bce37fb149c172330722f8bcdc2e54a2882e5
ksa_u <ksa_u@KHURAMSHAHZAD>
2017-05-19 17:13:44 +0500

    Added Books Index with Images

---
+++ DigitalLibrary/Controllers/BookController.cs
@@ -0,0 +1,58 @@
+﻿using DigitalLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalLibrary.Controllers
+{
+    public class BookController : Controller
+    {
+        [HttpGet]
+        // GET: Book
+        public ActionResult Index()
+        {
+            
+            return View(Book.GetAll());
+        }
+        [HttpGet]
+        public ActionResult Add()
+       {
+            
+            return View(new Book());
+        }
+        [HttpPost]
+        public ActionResult Add(Book book, HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                var allowedExtensions = new[] { ".jpeg", ".jpg", ".JPG", ".png" };
+                var filename = file.FileName.ToString();
+                var ext = Path.GetExtension(file.FileName);
+
+                if (allowedExtensions.Contains(ext))
+                {
+                    var name = Path.GetFileNameWithoutExtension(filename);
+                    var myfile = name + ext;
+                    var path = Path.Combine(Server.MapPath(@"~\Asserts\Book_Images"), myfile);
+                    book.BookCover = myfile;
+                    file.SaveAs(path);
+                }
+                if (Book.Save(book))
+                {
+                    return RedirectToAction("Index", "Book");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
--- DigitalLibrary/Models/Book.cs
+++ DigitalLibrary/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -14,9 +15,61 @@ namespace DigitalLibrary.Models
         public int AuthorId { get; set; }
         public int PublisherId { get; set; }
         public DateTime PublishingYear { get; set; }
+
+        internal static List<Book> GetAll()
+        {
+            Book books = new Book();
+            List<Book> BooksList = new List<Book>();
+            string query = "select * from Books";
+            Database_Helpers db = new Database_Helpers();
+            try
+            {
+                db.Connection.Open();
+                SqlCommand cmd = new SqlCommand(query, db.Connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        books = new Book();
+                        books.Id = (int)reader["Id"];
+                        books.Name = reader["Name"].ToString();
+                        books.AuthorId = (int)reader["AuthorId"];
+                        books.PublisherId = (int)reader["PublisherId"];
+                        books.PublishingYear = Convert.ToDateTime(reader["PublishingYear"].ToString());
+                        books.BookCompleted = Convert.ToByte(reader["BookCompleted"]);
+                        books.NoOfPages = Convert.ToInt16(reader["NoOfPages"]);
+                        books.NoOfAbwaabs= Convert.ToInt16(reader["NoOfAbwaabs"]);
+                        books.BookCover = reader["BookCover"].ToString();
+                        BooksList.Add(books);
+                    }
+                }
+                return BooksList;
+            }
+            catch (Exception ex)
+            {
+                db.Connection.Close();
+                return BooksList;
+            }
+        }
+
         public int EditionNumber { get; set; }
         public int BookCompleted { get; set; }
         public int NoOfPages { get; set; }
         public int NoOfAbwaabs { get; set; }
+        public string BookCover { get; set; }
+        
+        internal static bool Save(Book book)
+        {
+            Database_Helpers db = new Database_Helpers();
+            if(db.Insert("Books",book ))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
