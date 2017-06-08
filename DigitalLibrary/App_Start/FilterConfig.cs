d15f0f59ba3f5f076dff33e49fb7a678f8fd963c
ksa_u <ksa_u@KHURAMSHAHZAD>
2017-06-08 15:32:43 +0500

    Final Commit All has been done! :)
    Signed-off-by: ksa_u <ksa_u@KHURAMSHAHZAD>

--- DigitalLibrary/App_Start/FilterConfig.cs
+++ DigitalLibrary/App_Start/FilterConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using DigitalLibrary.Models;
+using System;
+using System.Web;
 using System.Web.Mvc;
 
 namespace DigitalLibrary
@@ -8,6 +10,24 @@ namespace DigitalLibrary
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            AddSeeds();
+        }
+
+        public static void AddSeeds()
+        {
+            Database_Helpers db = new Database_Helpers();
+            if (db.get_scalar("select Count(*) from users WHERE Name='Admin'") == 0)
+            {
+                Users _user = new Users();
+                _user.Name = "Admin";
+                _user.Password = "Admin1234%";
+                _user.Role = "admin";
+                _user.UserPhone = "03344037449";
+                _user.Location = "Jamia Ashrafia Lahore, Pakistan";
+                db.Insert("Users", _user);
+            }
+
+
         }
     }
 }
